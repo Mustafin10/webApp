f99f4eb697df0e53336da252da49a7407f9ca714
zolin22kirill <zolin22kirill@zolin22kirill-VirtualBox>
2021-05-07 19:58:54 +0500

    Создал метод "upak" и его реализацию

--- src/main/java/laba4/RasschetUpak.java
+++ src/main/java/laba4/RasschetUpak.java
@@ -5,7 +5,28 @@ package laba4;
 		public static int u;
 		public static int cupk;
 		
-		
+		public static int upak () {
+			
+			String upak1 = UserForm.upak1;
+			int v = Integer.parseInt(UserForm.v2);
+			
+			switch (upak1) {
+			case "1":
+				u=0;
+				break;
+			case "2":
+				u=10;
+				break;
+			case "3":
+				u=20;
+				break;
+			case "4":
+				u=30;
+				break;
+			}
+			
+			//cupk=Calc.Up(v, u);
+			return cupk;
 
 	}
-
+}
