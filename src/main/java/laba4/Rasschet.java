67b53ea7ed13408bfed1b1a670aececb88339d42
zolin22kirill <zolin22kirill@zolin22kirill-VirtualBox>
2021-05-07 22:04:11 +0500

    Изменения в классе "Rasschet". Добавление реализации промокодов.

--- src/main/java/laba4/Rasschet.java
+++ src/main/java/laba4/Rasschet.java
@@ -11,16 +11,30 @@ public class Rasschet {
 		
 		int v = Integer.parseInt(UserForm.v2);
 		int g = 0;
-		//int ves = Integer.parseInt(UserForm.ves);
-		//double s = SelectionCity.gorod();
+		int ves = Integer.parseInt(UserForm.ves);
+		double s = SelectionCity.gorod();
 		int c0 = Integer.parseInt(UserForm.c0);
 		int cdu = Integer.parseInt(UserForm.x) + Integer.parseInt(UserForm.y);
-		//int cupk = RasschetUpak.upak();
-		//String pom = UserForm.p21;
+		int cupk = RasschetUpak.upak();
+		String pom = UserForm.p21;
+		
+		if (pom.equals("2020")) {
+			p = 0.92;
+			kod="1";
+		}
+		else {
+			p = 1;
+			kod="0";
+		}
 		
 		
+		while (ves > 0 ||  v > 0) {
+			ves = ves - 1500; 
+			v = v - 9;
+			g = g + 1;
+		}
 
-		c = (int) Math.ceil((((g * 1) * ((/*s **/ 15) + (v * 100) + c0 * 0.2)) + /*cupk*/ + cdu) * p);
+		c = (int) Math.ceil((((g * 1) * ((s * 15) + (v * 100) + c0 * 0.2)) + cupk + cdu) * p);
 		return c;
 	}
 }
