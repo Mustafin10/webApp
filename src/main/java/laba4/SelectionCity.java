d98234cfe428f0ff8e5fe313b598baabeef6cfe1
Mustafin <sonyman2001@gmail.com>
2021-05-07 21:36:55 +0500

    Создал класс SelectionCity и реализовал выбор города.

---
+++ src/main/java/laba4/SelectionCity.java
@@ -0,0 +1,32 @@
+package laba4;
+
+public class SelectionCity {
+protected static double s=0;
+	
+	protected static double gorod() {
+		
+		String gorod = UserForm.gorod;
+		
+		switch (gorod) {
+		case "1":
+			s=189;
+			break;
+		case "2":
+			s=169;
+			break;
+		case "3":
+			s=163;
+			break;
+		case "4":
+			s=94.5;
+			break;
+		case "5":
+			s=139;
+			break;
+		}
+		
+		return s;
+		
+	}
+
+}
