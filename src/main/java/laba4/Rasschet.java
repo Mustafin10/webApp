6662575f2e751033e74730dccdeba0b84548437e
zolin22kirill <zolin22kirill@zolin22kirill-VirtualBox>
2021-05-07 19:51:06 +0500

    Создал метод "calcul" и добавил основную формулу с дополнительными переменными

--- src/main/java/laba4/Rasschet.java
+++ src/main/java/laba4/Rasschet.java
@@ -6,5 +6,22 @@ public class Rasschet {
 	protected static int cdu;
 	protected static double p;
 	protected static String kod;
+	
+	protected static int calcul () {
+		
+		int v = Integer.parseInt(UserForm.v2);
+		int g = 0;
+		//int ves = Integer.parseInt(UserForm.ves);
+		//double s = SelectionCity.gorod();
+		int c0 = Integer.parseInt(UserForm.c0);
+		int cdu = Integer.parseInt(UserForm.x) + Integer.parseInt(UserForm.y);
+		//int cupk = RasschetUpak.upak();
+		//String pom = UserForm.p21;
+		
+		
 
+		c = (int) Math.ceil((((g * 1) * ((/*s **/ 15) + (v * 100) + c0 * 0.2)) + /*cupk*/ + cdu) * p);
+		return c;
+	}
 }
+
