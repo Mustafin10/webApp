d324b943681c982aaedd35f979658ed87ddef082
Makarov <egor.ma01@mail.ru>
2021-05-12 01:22:27 +0500

    Изменил класс AdminForm и добавил комментарии к коду

--- src/main/java/laba4/AdminForm.java
+++ src/main/java/laba4/AdminForm.java
@@ -1,12 +1,11 @@
 package laba4;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,32 +14,49 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name="adminForm", urlPatterns="/AdminForm")
 public class AdminForm extends HttpServlet {
-
+    //Объявление переменных
 	protected static String subject;
 	protected static String add_promo;
 	protected static String services;
-	public BufferedReader br;
 	public static String gt;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		subject = request.getParameter("subject");
-		add_promo = request.getParameter("add_promo");
-		services = request.getParameter("services");
+		subject = request.getParameter("subject"); //Получение значения из поля subject
+		add_promo = request.getParameter("add_promo"); //Получение значения из поля add_promo
+            
+            try {
+            	//Создание и запись значений в файл
+				File file = new File("promo.txt");
+				
+				if(!file.exists())
+				file.createNewFile();
+				
+				BufferedWriter pw = new BufferedWriter(new FileWriter(file, true));
+				String lineSeparator = System.getProperty("line.separator");
+				pw.write(add_promo+ lineSeparator);
+				pw.flush();
+				pw.close();
+				
+				//Чтение данных из файла
+				
+				FileReader fr = new FileReader(file);
+	            //создаем BufferedReader с существующего FileReader для построчного считывания
+	            BufferedReader br = new BufferedReader(fr);
+	            // считаем сначала первую строку
+	            String line;
 
-		FileWriter writer = new FileWriter("promo.txt", false);
-		 
-           // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-             
-            writer.flush();
-		
-		System.out.println(add_promo);
+					while((line = br.readLine())!=null) {
+						gt = line;
+					}
+							
+			}catch(IOException e1){
+			     e1.printStackTrace();
+			}
+            
 		request.getRequestDispatcher("/adminForm.jsp").forward(request, response);
 		
 	}
 }
+
+
 
