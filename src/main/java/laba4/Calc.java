1529d390ba641b089e4fb04b93aac3ea95747ed6
Makarov <egor.ma01@mail.ru>
2021-05-12 00:44:59 +0500

    Именил структуру программного кода

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -2,10 +2,11 @@ package laba4;
 
 public abstract class Calc {
 	
-public abstract  int  calcul () ;
-public static int reckonUpk (int u,int v) {
-	int cupk;
-	cupk = u*v;
-	return cupk;
-}	
-}
+	public abstract  int  calcul () ;
+	
+	public static int reckonUpk (int u,int v) {
+		int cupk;
+		cupk = u*v;
+		return cupk;
+		}	
+	}
