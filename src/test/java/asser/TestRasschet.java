6b5b77f9251bf476627bd7d61340b53520a1fffb
Zolin <zolin2001@bk.ru>
2021-05-15 05:58:39 +0500

    Добавил директории для тестов (src/test/java/asser). А также создал тест класса Rasschet.

---
+++ src/test/java/asser/TestRasschet.java
@@ -0,0 +1,17 @@
+package asser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import laba4.Rasschet;
+
+public class TestRasschet {
+
+	@Test
+	public void test() {
+		Assert.assertNotNull(Rasschet.c);
+	}
+
+}
