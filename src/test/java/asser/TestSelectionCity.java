44b327fe789cfaa924266c6d7e9728d7bcae4a03
Mustafin10 <79744075+Mustafin10@users.noreply.github.com>
2021-05-15 06:55:47 +0500

    изменил тест для класса SelectionCity-TestSelectionCity

--- src/test/java/asser/TestSelectionCity.java
+++ src/test/java/asser/TestSelectionCity.java
@@ -2,13 +2,26 @@ package asser;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import laba4.SelectionCity;
+
 public class TestSelectionCity {
+	
+	String expectedString;
+	boolean expectedBoolean;
+	
+	@Before
+	public void pretest() {
+		assertNotNull(SelectionCity.s);
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		assertNotSame(SelectionCity.s, expectedString);
+		assertNotSame(SelectionCity.s, expectedBoolean);
 	}
 
 }
